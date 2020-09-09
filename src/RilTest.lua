e23b3a97ff1891ee67993992bea948d67570ea0c
yanjunjie <hashjeremy@163.com>
2020-09-09 16:09:44 +0800

    updte

--- src/RilTest.lua
+++ src/RilTest.lua
@@ -5,9 +5,12 @@
 
 module(...,package.seeall)
 
+local rilTestCount = 1
+
 sys.taskInit(
     function()
         while true do
+            log.info("RilTest.rilTestCount", "第"..rilTestCount.."次测试开始")
             ril.request("AT+CSQ")
             sys.wait(2000)
             ril.request("AT+CGDCONT?")
@@ -16,6 +19,8 @@ sys.taskInit(
             sys.wait(2000)
             ril.request("AT+EEMGINFO?")
             sys.wait(2000)
+            log.info("RilTest.rilTestCount", "第"..rilTestCount.."次测试完成")
+            rilTestCount = rilTestCount + 1
         end
     end
 )
