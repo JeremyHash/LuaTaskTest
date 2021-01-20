01e8893f8b504e3c2323489048d442e00ccf7e3f
zhaojiuri <1298914543@qq.com>
2021-01-20 15:06:58 +0800

    update cfg

--- src/cfg/OneDeviceOneSecret.lua
+++ src/cfg/OneDeviceOneSecret.lua
@@ -5,8 +5,16 @@
 
 module(..., package.seeall)
 
+-- Jeremy
+-- AliyunInfo = {
+--     ["PRODUCT_KEY"] = "a1K4TWp6E6z",
+--     ["DEVICE_NAME"] = "866714049394322",
+--     ["DEVICE_SECRET"] = "a044c473cdb320c9b698a3592af0e762"
+-- }
+
+-- 赵旭
 AliyunInfo = {
-    ["PRODUCT_KEY"] = "a1K4TWp6E6z",
-    ["DEVICE_NAME"] = "866714049394322",
-    ["DEVICE_SECRET"] = "a044c473cdb320c9b698a3592af0e762"
+    ["PRODUCT_KEY"] = "a1HvZlnlln9",
+    ["DEVICE_NAME"] = "866262047559884",
+    ["DEVICE_SECRET"] = "25cd3b2cf0b527200e3979b6732aca12"
 }
